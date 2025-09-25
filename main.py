99870bad9927b913072305a62604134f22ca3d9d
vladimirsh <shamirzaev.vladimir@mail.ru>
2025-09-25 17:06:07 +0700

    Написан вывод отчёта в консоль

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from collections import defaultdict
 import tabulate
 
 
+def print_report(report_dict: dict) -> None:
+    rows = [(student, avg) for student, avg in report_dict.items()]
+    print(tabulate.tabulate(rows, headers=['Student', 'Average Grade'], tablefmt="pipe"))
+
+
 def open_file(files: str) -> dict:
     student_dict = defaultdict(list)
     for file in files:
-        with open(file, 'r', encoding='Utf-8', newline='') as csvfile:
-            file = csv.DictReader(csvfile)
-            for row in file:
-                student = row['student_name']
-                grade = int(row['grade'])
-                student_dict[student].append(grade)
-
+        file = csv.DictReader(file)
+        for row in file:
+            student = row['student_name']
+            grade = int(row['grade'])
+            student_dict[student].append(grade)
     return student_dict
 
 
@@ -24,11 +27,17 @@ def average_grade(student_dict: dict) -> dict:
     sorted_dict = dict(sorted(average_dict.items(), key=lambda i: i[1], reverse=True))
     return sorted_dict
 
+
 parser = argparse.ArgumentParser(description='student performance')
-parser.add_argument('--files', dest='files', nargs='+')
-# parser.add_argument('--report', dest='report')
+parser.add_argument('--files', dest='files', nargs='+', type=argparse.FileType(mode='r', encoding='utf-8'))
+parser.add_argument('--report', dest='report')
 
 args = parser.parse_args()
+student_grades = open_file(files=args.files)
+student_report = average_grade(student_dict=student_grades)
 
-open_file(files=args.files)
-average_grade(open_file(files=args.files))
+if args.report == 'student-performance':
+    print_report(report_dict=student_report)
+
+
+
